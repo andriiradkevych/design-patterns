5332fea357c5d6eaccef60a637f25b7ad6a8d958
Andrii Radkevych <radkevychandrii7@gmail.com>
2024-07-25 18:07:09 +0300

    Strategy Pattern

---
+++ behavioral/strategy/index.ts
@@ -0,0 +1,53 @@
+type TSort = number[];
+
+interface ISortingStraregy {
+  sort: (arr: TSort) => void;
+}
+
+class Sorting {
+  sortingStrategy: ISortingStraregy;
+
+  constructor(sortingStrategy: ISortingStraregy) {
+    this.sortingStrategy = sortingStrategy;
+  }
+
+  changeSortingStrategy(sortingStrategy: ISortingStraregy) {
+    this.sortingStrategy = sortingStrategy;
+  }
+
+  sort(arr: TSort) {
+    this.sortingStrategy.sort(arr);
+  }
+}
+
+class BubbleSortStrategy implements ISortingStraregy {
+  sort(arr: TSort) {
+    console.log(arr, "Bubble sort");
+  }
+}
+
+class MergeSortStrategy implements ISortingStraregy {
+  sort(arr: TSort) {
+    console.log(arr, "Merge sort");
+  }
+}
+
+class QuickSortStrategy implements ISortingStraregy {
+  sort(arr: TSort) {
+    console.log(arr, "Quick sort");
+  }
+}
+
+const sortArray = [4, 2, 1, 3];
+
+const sorting = new Sorting(new BubbleSortStrategy());
+
+sorting.sort(sortArray); // Bubble sort
+
+sorting.changeSortingStrategy(new MergeSortStrategy());
+
+sorting.sort(sortArray); // Merge sort
+
+sorting.changeSortingStrategy(new QuickSortStrategy());
+
+sorting.sort(sortArray); // Quich sort
